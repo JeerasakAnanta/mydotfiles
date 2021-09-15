2d552e8fd7bcdefd0510fb9e3e05aeb067ec8821
Game <jeerasakananta@gmail.com>
2021-09-15 09:47:50 +0700

    mod install auto format space

--- instasll.py
+++ instasll.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os 
+import os
 import sys
 # from whichcraft import which
 from shutil import which
+
 
 def is_tools(name):
     return which(name) is not None
 
 
 def install(name):
-    os.system("sudo apt install"+ name)
+    os.system("sudo apt install" + name)
+
 
 def main():
     print("=============================================")
@@ -19,22 +21,22 @@ def main():
     print("=============================================")
 
     if not os.geteuid() == 0:
-        while(True):
+        while (True):
 
-            print("contnunt install y:N",end=" >")
+            print("contnunt install y:N", end=" >")
             inputin = input()
-            if(inputin == "y"):
+            if (inputin == "y"):
                 os.system("sudo -i ")
-            if(inputin == "N"):
+            if (inputin == "N"):
                 break
     else:
         name = ["git", "python3", "efh", "neovim"]
         for i in name:
-            if(is_tools(i) != True):
+            if (is_tools(i) != True):
                 install(i)
             else:
                 print(i + "is in install :)")
 
+
 if __name__ == '__main__':
     main()
-
