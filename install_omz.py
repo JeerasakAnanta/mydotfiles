2eccb05e2ef9e7c6e91c698c61d80734a1ca8253
Game <jeerasakananta@gmail.com>
2024-12-20 13:32:09 +0700

    init :  install oh my zsh script

---
+++ install_omz.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+# code by Jeersask ananta 
+# last update 20/12/2024 
+
+
+import os
+import sys
+from shutil import which
+
+def git_clone(repo_url, destination=None):
+    """Clone a git repository to a specified destination."""
+    command = "git clone {}".format(repo_url)
+    if destination:
+        command += " {}".format(destination)
+    os.system(command)
+
+
+def install_zsh():
+    """Install zsh using apt package manager."""
+    os.system("sudo apt install zsh")
+
+
+def main():
+    
+    # cehck zsh 
+    if not which("zsh"):
+        install_zsh()
+    print("install oh-my-zsh Done")
+    
+    
+    # call function git clone 
+    
+    # git_clone("https://github.com/ohmyzsh/ohmyzsh.git", "~/.oh-my-zsh")
+    
+    
+if __name__ == "__main__":
+    main()
+    
+        
+        
